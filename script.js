9f329452816cc2a93c7ec158f17e72056ab7edf7
Beatriz Braganholo <104452213+biabraganholo@users.noreply.github.com>
2024-09-26 11:44:20 -0300

    script.js

--- script.js
+++ script.js
@@ -1,70 +1,17 @@
-const perguntas = [
-    {
-        enunciado: "qual sua cor favorita?",
-        alternativas: [
-            { texto: " azul!", pontuacao: 1 },
-            { texto: "verde!", pontuacao: 2 }
-        ]
-    },
-    {
-        enunciado: "qual numero vc prefere?",
-        alternativas: [
-            { texto: "12.", pontuacao: 2 },
-            { texto: "7.", pontuacao: 1 }
-        ]
-    },
-    {
-        enunciado: "qual sua fruta preferida?",
-        alternativas: [
-            { texto: "maça.", pontuacao: 2 },
-            { texto: "laranja.", pontuacao: 1 }
-        ]
-    },
-    {
-        enunciado: "qual?",
-        alternativas: [
-            { texto: "quimica.", pontuacao: 1 },
-            { texto: "fisica.", pontuacao: 2 }
-        ]
-    },
-    {
-        enunciado: "vc ama Jesus?",
-        alternativas: [
-            { texto: "sim muito.", pontuacao: 1 },
-            { texto: "sim muitao.", pontuacao: 2 }
-        ]
-    }
-];
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+   
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+    console.log(paragrafo)
 
-let atual = 0;
-let pontuacao = 0; // Variável para armazenar a pontuação
-let perguntaAtual;
-const caixaPerguntas = document.getElementById('caixaPerguntas');
-const alternativasContainer = document.getElementById('alternativas');
-
-function mostraPergunta() {
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    alternativasContainer.innerHTML = '';
-    perguntaAtual.alternativas.forEach((alternativa) => {
-        const button = document.createElement('button');
-        button.textContent = alternativa.texto;
-        button.addEventListener('click', () => {
-            pontuacao += alternativa.pontuacao; // Incrementa a pontuação com base na alternativa selecionada
-            atual++;
-            if (atual < perguntas.length) {
-                mostraPergunta();
-            } else {
-                exibePontuacaoFinal(); // Chama função para exibir a pontuação final
-            }
-        });
-        alternativasContainer.appendChild(button);
-    });
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
 
-function exibePontuacaoFinal() {
-    caixaPerguntas.textContent = `Você completou o questionário! Sua pontuação final é: ${pontuacao}`;
-    alternativasContainer.innerHTML = ''; // Limpa as alternativas
-}
+vizualizarInformacoesGlobais()
 
-mostraPergunta(); // Inicia o questionário mostrando a primeira pergunta
+
